db364ac417df358a921edf9bbf51ef4b9b20e7e7
radl97 <radl97@users.noreply.github.com>
2021-11-04 00:43:03 +0100

    add checks for CAFF_HEADER too

--- parser/Parser/CAFF_HEADER.h
+++ parser/Parser/CAFF_HEADER.h
@@ -12,7 +12,13 @@ public:
     void read(Reader& r) {
         Block::read(r);
         r.readArray(magic, 4);
+		if (magic[0] != 'C' || magic[1] != 'A' || magic[2] != 'F' || magic[3] != 'F') {
+			throw ParsingException();
+		}
         r.readPrimitive(headerSize);
+        if (headerSize != sizeof(magic) + sizeof(headerSize) + sizeof(animationNumber)) {
+            throw ParsingException();
+        }
         r.readPrimitive(animationNumber);
     }
 };
